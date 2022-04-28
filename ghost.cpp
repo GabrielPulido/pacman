fe8acb7fa7d71b4e0fafd2938d8ccd4262e689ca
Noorulain Irshad <56182580+noorulainirshad@users.noreply.github.com>
2022-04-28 12:03:20 -0700

    updating current pathfinding

--- ghost.cpp
+++ ghost.cpp
@@ -11,21 +11,22 @@ Ghost::Ghost(float x, float y, float r, float g, float b) : Character(x,y)
     setColor(r,g,b);
 }
 
-//generates a random number & based on that random number, it decides the direction the ghost will go
-//if we implement path-finding algorithm this will be deleted
-void Ghost::randomizeDirection()
+//setting up a ghost that follows pacman
+void Ghost::redDirection(Character pacman, Grid &grid)
 {
-    int randomNumber = rand() % 5 + 2;
-    if(randomNumber == 2) {
-        direction = right;
-    }
-    if(randomNumber == 3) {
-        direction = up;
-    }
-    if(randomNumber == 4) {
-        direction = left;
-    }
-    if(randomNumber == 5) {
-        direction = down;
-    }
+   int newX = pacman.getx();
+   int newY = pacman.gety();
+  // int oldX, oldY;
+   int hitwall = 0;
+
+   for(int i = 0; i <= newY; i++){
+       if (grid.squares[int(this->getx())][newY].getBoundary() == false)
+       {
+           this->sety(i);
+           hitwall++;
+
+       }
+
+   }
 }
+
