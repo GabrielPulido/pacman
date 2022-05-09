cb7ee8be1de75446261726b4185b4229bd20b62f
Noorulain Irshad <56182580+noorulainirshad@users.noreply.github.com>
2022-05-09 15:09:09 -0700

    Update ghost.h

--- ghost.h
+++ ghost.h
@@ -2,13 +2,19 @@
 #define GHOST_H
 
 #include "character.h"
+#include "grid.h"
 
 class Ghost : public Character
 {
 public:
+
     Ghost();
     Ghost(float, float, float, float, float);
     void redDirection(Character pacman, Grid &grid);
+    void pinkDirection(Character pacman, Grid &grid);
+    void blueDirection(Character pacman, Grid &grid);
+    void orangeDirection(Character pacman, Grid &grid);
+
 };
 
 #endif // GHOST_H
